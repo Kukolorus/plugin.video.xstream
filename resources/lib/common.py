18f804b0ffae55e2b3b3d7173b4470243fbd7d42
Michael Langfermann <versable@gmail.com>
2016-01-10 14:25:18 +0100

    Remove settings.xml completly from git, let xStream generate it

--- resources/lib/common.py
+++ resources/lib/common.py
@@ -5,4 +5,4 @@ addonID = 'plugin.video.xstream'
 addon = xbmcaddon.Addon(id = addonID)
 addonPath = translatePath(addon.getAddonInfo('path')).decode('utf-8')
 profilePath = translatePath(addon.getAddonInfo('profile')).decode('utf-8')
-settings_file = os.path.join(addonPath, 'resources', 'settings_test.xml')
+settings_file = os.path.join(addonPath, 'resources', 'settings.xml')
